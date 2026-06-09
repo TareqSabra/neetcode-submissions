7e799025c164cba9cefddd1df511ef27800aecb6
TareqSabra <33294386+TareqSabra@users.noreply.github.com>
2026-06-09 11:13:41 +0300

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        // prefix sum find left and right array add them togther 
+        const len = nums.length;
+        const leftArray = new Array(len);
+        const rightArray = new Array(len);
+        leftArray[0] = 1;
+        for (let i = 1; i < len; i++) {
+            leftArray[i] = leftArray[i - 1] * nums[i - 1];
+        }
+        rightArray[len - 1] = 1;
+        for (let j = len - 2; j >= 0; j--) {
+            rightArray[j] = rightArray[j + 1] * nums[j + 1];
+        }
+        const finalArray = leftArray.map((leftVal, index) =>
+            leftVal * rightArray[index]
+        );
+        return finalArray
+    }
+}
