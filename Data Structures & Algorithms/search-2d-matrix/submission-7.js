8e9d881d7c862cfa8737c3ea8c028288a93c1491
TareqSabra <33294386+TareqSabra@users.noreply.github.com>
2026-06-14 12:25:02 +0300

    Add: search-2d-matrix - submission-7

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-7.js
@@ -0,0 +1,37 @@
+
+function binarySearch(array, target) {
+    let start = 0
+    let end = array.length - 1
+    while (start <= end) {
+        let midIndex = Math.floor((start + end) / 2)
+        const currentGuess = array[midIndex]
+        if (currentGuess === target) {
+            return true
+        }
+        if (target < currentGuess) {
+            end = midIndex - 1
+        }
+        if (target > currentGuess) {
+            start = midIndex + 1
+        }
+    }
+    return 0
+}
+
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+
+    searchMatrix(matrix, target) {
+        for (const array of matrix) {
+            const isFound = binarySearch(array, target)
+            if (isFound) {
+                return true
+            }
+        }
+        return false
+    }
+}
